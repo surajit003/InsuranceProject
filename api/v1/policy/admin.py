9362eda627c79e24345d13412631f2f43fb9e7a2
Surajit Das <surajit.das0320@gmail.com>
2022-12-17 16:33:44 +0300

    add policy model

--- api/v1/policy/admin.py
+++ api/v1/policy/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
 # Register your models here.
+from api.v1.policy.models import Policy
+
+
+@admin.register(Policy)
+class PolicyAdmin(admin.ModelAdmin):
+    model = Policy
+    readonly_fields = (
+        "uuid",
+        "created",
+        "modified",
+        "state",
+    )
+    search_fields = [
+        "id",
+        "type",
+        "customer",
+    ]
+    list_display = [
+        "id",
+        "type",
+        "cover",
+        "customer",
+        "state",
+    ]
+    list_filter = [
+        "state",
+        "customer",
+        "modified",
+    ]
--- api/v1/policy/models.py
+++ api/v1/policy/models.py
@@ -1,3 +1,33 @@
+from simple_history.models import HistoricalRecords
+
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+from api.v1.common.models import UUIDModel, TimeTrackedModel
+from api.v1.customer.models import Customer
+
+
+class Policy(TimeTrackedModel, UUIDModel):
+    STATUS_NEW = 0
+    STATUS_ACCEPTED = 1
+    STATUS_ACTIVE = 2
+
+    POLICY_STATUS_CHOICES = (
+        (STATUS_NEW, "New"),
+        (STATUS_ACCEPTED, "Accepted"),
+        (STATUS_ACTIVE, "Active"),
+    )
+
+    type = models.CharField(_("Policy Type"), max_length=120)
+    premium = models.IntegerField(_("Premium Amount"))
+    cover = models.IntegerField(_("Cover"))
+    state = models.PositiveSmallIntegerField(
+        choices=POLICY_STATUS_CHOICES, default=STATUS_NEW, db_index=True
+    )
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, db_index=True)
+
+    history = HistoricalRecords()
+
+    def __str__(self):
+        return f"Policy for {self.customer} for cover {self.cover}"
