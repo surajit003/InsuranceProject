93b110ba674f298ead209c705892a2ecef3710a1
Surajit Das <surajit.das0320@gmail.com>
2022-12-20 21:49:08 +0300

    refactor signal logic for policy calculation

--- api/v1/policy/signals.py
+++ api/v1/policy/signals.py
@@ -5,7 +5,17 @@ from api.v1.policy.models import Policy
 
 
 @receiver(post_save, sender=Policy)
-def trigger_payment(sender, instance, created, **kwargs):
+def trigger_payment(sender, instance, **kwargs):
+    policy = Policy.objects.filter(id=instance.id).first()
+    if policy.state == Policy.STATUS_ACCEPTED:
+        print("Payment has been triggered...")
+        print("Simulating customer has made the payment....")
+        policy.state = Policy.STATUS_ACTIVE
+        policy.save()
+
+
+@receiver(post_save, sender=Policy)
+def policy_calculation(sender, instance, created, **kwargs):
     if created:
         # just a dummy policy criteria based on age
         age = instance.customer.age
@@ -14,10 +24,3 @@ def trigger_payment(sender, instance, created, **kwargs):
         instance.cover = cover
         instance.premium = premium
         instance.save()
-
-    policy = Policy.objects.filter(id=instance.id).first()
-    if policy.state == Policy.STATUS_ACCEPTED:
-        print("Payment has been triggered...")
-        print("Simulating customer has made the payment....")
-        policy.state = Policy.STATUS_ACTIVE
-        policy.save()
